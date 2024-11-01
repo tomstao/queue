846ff9d42fe63fde39a1c3992345499864730bb4
tomstao <96sutao@gmail.com>
2024-11-01 10:53:08 -0400

    queue

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <string>
 
 // Queue is fifo data structure, it means first in first serve.
 
@@ -11,6 +12,10 @@ void printQueue(std::queue<int> q)
         q.pop();
     }
 }
+
+void addTask (std::string addingTask, std::queue<std::string>);
+void removeTask (std::string removingTask, std::queue<std::string>);
+
 
 int main()
 {
@@ -24,6 +29,26 @@ int main()
     std::cout << "back: " << myqueue.back() << std::endl;
 
     printQueue(myqueue);
+    std::cout << "******************************************************" << std::endl;
+    std::queue<std::string> dailyTasks;
+
     
     return 0;
+}
+
+void addTask (std::string addingTask, std::queue<std::string> tasks)
+{
+    tasks.push(addingTask);
+    std::cout << "You added the task: " << addingTask <<" successfully!" <<std::endl;
+    std::cout << "You have " << tasks.size() << " tasks left!" << std::endl;
+}
+
+void removeTask ( std::queue<std::string> tasks)
+{
+    std::cout << "You removed the task: " << tasks.front() << std::endl;
+    tasks.pop();
+    if(tasks.empty())
+    {
+        std::cout << "The tasks are done!" << std::endl;
+    }
 }
